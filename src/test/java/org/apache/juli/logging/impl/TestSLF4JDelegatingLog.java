7df103d7601910b7546ace277ef5e5ab7e274ed4
Benjamin Gandon <benjamin@gandon.org>
2017-06-22 00:46:31 +0200

    Update copyright years

--- src/test/java/org/apache/juli/logging/impl/TestSLF4JDelegatingLog.java
+++ src/test/java/org/apache/juli/logging/impl/TestSLF4JDelegatingLog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Benjamin Gandon
+ * Copyright 2015-2017 Benjamin Gandon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
